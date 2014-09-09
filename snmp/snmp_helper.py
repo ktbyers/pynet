8f55c0a63a40853ca4f089ab8da6dc1f43edd8e7
AWS Dev <ktbyers@twb-tech.com>
2014-09-09 11:54:22 -0700

    Adding SNMPv3 get oid function

--- snmp/snmp_helper.py
+++ snmp/snmp_helper.py
@@ -1,7 +1,7 @@
 '''
-Requires the pysnmp version4 library
+Requires the pysnmp4 library
 
-Example usage:
+Example usage (SNMPv1/SNMPv2c):
 
 >>> from snmp_helper import snmp_get_oid,snmp_extract
 >>> 
@@ -29,9 +29,119 @@ Technical Support: http://www.cisco.com/techsupport
 Copyright (c) 1986-2010 by Cisco Systems, Inc.
 Compiled Fri 29-Oct-10 00:02 by prod_rel_team
 
+
+Example usage (SNMPv3):
+
+>>> from snmp_helper import snmp_get_oid_v3,snmp_extract
+>>>
+>>> snmp_device = ('10.10.10.10', 161)
+
+>>> a_user = <snmpv3_user>
+>>> auth_key = <snmpv3_auth_key>
+>>> encrypt_key = <snmpv3_encrypt_key>
+>>> snmp_user = (a_user, auth_key, encrypt_key)
+
+OID to query
+>>> sys_descr = '1.3.6.1.2.1.1.1.0'
+
+Defaults to using AES128 and SHA1
+>>> snmp_data = snmp_get_oid_v3(snmp_device, snmp_user, oid=sys_descr)
+>>> output = snmp_extract(snmp_data)
+
+>>> print output
+Cisco IOS Software, C880 Software (C880DATA-UNIVERSALK9-M), Version 15.4(2)T1, RELEASE SOFTWARE (fc3)
+Technical Support: http://www.cisco.com/techsupport
+Copyright (c) 1986-2014 by Cisco Systems, Inc.
+Compiled Thu 26-Jun-14 14:15 by prod_rel_team
+
 '''
 
 from pysnmp.entity.rfc3413.oneliner import cmdgen
+
+
+def snmp_get_oid_v3(snmp_device, snmp_user, oid='.1.3.6.1.2.1.1.1.0', auth_proto='sha', encrypt_proto='aes128', display_errors=True):
+    '''
+    Retrieve the given OID
+
+    Default OID is MIB2, sysDescr
+
+    a_device is a tuple = (a_host, snmp_port)
+    a_user is a tuple = (user_name, auth_key, encrypt_key)
+
+    Defaults to SHA1-AES128 for authentication + encryption
+   
+    auth_proto can be 'sha' or 'md5' or 'none'
+    encrypt_proto can be 'aes128', 'aes192', 'aes256', '3des', 'des', or 'none'
+ 
+
+    From PySNMP manuals:  http://pysnmp.sourceforge.net/docs/current/security-configuration.html
+
+    Optional authProtocol parameter may be used to specify non-default hash function algorithm. Possible values include:
+    usmHMACMD5AuthProtocol -- MD5-based authentication protocol
+    usmHMACSHAAuthProtocol -- SHA-based authentication protocol
+    usmNoAuthProtocol -- no authentication to use (default)
+
+    Optional privProtocol parameter may be used to specify non-default ciphering algorithm. Possible values include:
+    usmDESPrivProtocol -- DES-based encryption protocol
+    usmAesCfb128Protocol -- AES128-based encryption protocol (RFC3826)
+    usm3DESEDEPrivProtocol -- triple DES-based encryption protocol (Extended Security Options)
+    usmAesCfb192Protocol -- AES192-based encryption protocol (Extended Security Options)
+    usmAesCfb256Protocol -- AES256-based encryption protocol (Extended Security Options)
+    usmNoPrivProtocol -- no encryption to use (default)
+
+    '''
+
+    # unpack snmp_user
+    a_user, auth_key, encrypt_key = snmp_user
+
+    auth_proto_map = {
+        'sha':  cmdgen.usmHMACSHAAuthProtocol,
+        'md5':  cmdgen.usmHMACMD5AuthProtocol,
+        'none': cmdgen.usmNoAuthProtocol
+    }
+
+    if auth_proto in auth_proto_map.keys():
+        auth_protocol = auth_proto_map[auth_proto]
+    else:
+        raise ValueError("Invalid authentication protocol specified: %s" % auth_proto)
+
+    encrypt_proto_map = {
+        'des':      cmdgen.usmDESPrivProtocol,
+        '3des':     cmdgen.usm3DESEDEPrivProtocol,
+        'aes128':   cmdgen.usmAesCfb128Protocol,
+        'aes192':   cmdgen.usmAesCfb192Protocol,
+        'aes256':   cmdgen.usmAesCfb256Protocol,
+        'none':     cmdgen.usmNoPrivProtocol,
+    }
+
+    if encrypt_proto in encrypt_proto_map.keys():
+        encrypt_protocol = encrypt_proto_map[encrypt_proto]
+    else:
+        raise ValueError("Invalid encryption protocol specified: %s" % encrypt_proto)
+
+
+    # Create a PYSNMP cmdgen object
+    cmd_gen = cmdgen.CommandGenerator()
+
+    (error_detected, error_status, error_index, snmp_data) = cmd_gen.getCmd(
+
+        cmdgen.UsmUserData(a_user, auth_key, encrypt_key,
+            authProtocol=auth_protocol,
+            privProtocol=encrypt_protocol, ),
+        cmdgen.UdpTransportTarget(snmp_device),
+        oid,
+        lookupNames=True, lookupValues=True
+    )
+
+    if not error_detected:
+        return snmp_data
+    else:
+        if display_errors:
+            print 'ERROR DETECTED: '
+            print '    %-16s %-60s' % ('error_message', error_detected)
+            print '    %-16s %-60s' % ('error_status', error_status)
+            print '    %-16s %-60s' % ('error_index', error_index)
+        return None
 
 
 def snmp_get_oid(a_device, oid='.1.3.6.1.2.1.1.1.0', display_errors=False):
