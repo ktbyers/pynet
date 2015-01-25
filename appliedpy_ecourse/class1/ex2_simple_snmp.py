e424a49bec3571bb626c5ef32c93c0a6b56944db
Kirk Byers <ktbyers@twb-tech.com>
2015-01-25 15:30:23 -0800

    Cleaning up slightly

--- appliedpy_ecourse/class1/ex2_simple_snmp.py
+++ appliedpy_ecourse/class1/ex2_simple_snmp.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 '''
 Applied Python Course, Class1, Exercise 2c
 
@@ -6,22 +7,22 @@ Note, you will need to update the IP and COMMUNITY_STRING to use this script.
 '''
 
 import snmp_helper
- 
-COMMUNITY_STRING = '*******'
-IP = '10.10.10.10'
 
-pynet_rtr1 = (IP, COMMUNITY_STRING, 7961)
-pynet_rtr2 = (IP, COMMUNITY_STRING, 8061)
+COMMUNITY_STRING = '********'
+ip_addr = '10.10.10.10'
 
-sys_descr = '1.3.6.1.2.1.1.1.0'
-sys_name = '1.3.6.1.2.1.1.5.0'
+pynet_rtr1 = (ip_addr, COMMUNITY_STRING, 7961)
+pynet_rtr2 = (ip_addr, COMMUNITY_STRING, 8061)
+
+SYS_DESCR = '1.3.6.1.2.1.1.1.0'
+SYS_NAME = '1.3.6.1.2.1.1.5.0'
 
 
 for a_device in (pynet_rtr1, pynet_rtr2):
 
     print "\n*********************"
-    for the_oid in (sys_name, sys_descr):
-        snmp_data = snmp_helper.snmp_get_oid(a_device, oid=the_oid) 
+    for the_oid in (SYS_NAME, SYS_DESCR):
+        snmp_data = snmp_helper.snmp_get_oid(a_device, oid=the_oid)
         output = snmp_helper.snmp_extract(snmp_data)
 
         print output
