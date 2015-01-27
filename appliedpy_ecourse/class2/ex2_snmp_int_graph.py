ca288b7c74a2e94cc61ad52799221b273a3be61a
Kirk Byers <ktbyers@twb-tech.com>
2015-01-26 16:33:08 -0800

    pylint cleanup

--- appliedpy_ecourse/class2/ex2_snmp_int_graph.py
+++ appliedpy_ecourse/class2/ex2_snmp_int_graph.py
@@ -2,13 +2,13 @@
 https://pynet.twb-tech.com
 Applied Python, Class2, Exercise2
 
-Note, you will need to update the IP, SNMP username and keys to use this script.
+Note, you will need to update the ip_addr, SNMP username and keys to use this script.
 
 '''
 
-from snmp_helper import snmp_get_oid_v3,snmp_extract
+from snmp_helper import snmp_get_oid_v3, snmp_extract
 import line_graph
-import time 
+import time
 
 
 def get_interface_stats(snmp_device, snmp_user, stat_type, row_number):
@@ -48,18 +48,17 @@ def main():
 
     DEBUG = False
 
-    IP = '1.1.1.1'
-    
-    a_user = 'someuser'
-    auth_key = '*********'
-    encrypt_key = '*************'
-    
+    ip_addr = '10.10.10.10'
+
+    a_user = 'username'
+    auth_key = '********'
+    encrypt_key = '********'
+
     snmp_user = (a_user, auth_key, encrypt_key)
-    
-    pynet_rtr1 = (IP, 7961)
-    pynet_rtr2 = (IP, 8061)
-    snmp_device = pynet_rtr1 
-    
+
+    pynet_rtr1 = (ip_addr, 7961)
+    snmp_device = pynet_rtr1
+
     # Fa4 is in row number5 in the MIB-2 interfaces table
     row_number = 5
 
@@ -71,9 +70,9 @@ def main():
     }
     base_count_dict = {}
 
-    # Enter a loop gathering SNMP data every 5 minutes for an hour. 
-    for time_track in range(0,65,5):
-    
+    # Enter a loop gathering SNMP data every 5 minutes for an hour.
+    for time_track in range(0, 65, 5):
+
         print "\n%20s %-60s" % ("time", time_track)
 
         # Gather SNMP statistics for these four fields
@@ -82,7 +81,7 @@ def main():
             # Retrieve the SNMP data
             snmp_retrieved_count = get_interface_stats(snmp_device, snmp_user, entry, row_number)
 
-            # Get the base counter value 
+            # Get the base counter value
             base_count = base_count_dict.get(entry)
             if base_count:
                 # Save the data to graph_stats dictionary
@@ -96,26 +95,30 @@ def main():
 
 
     print
-    if DEBUG: print graph_stats
+    if DEBUG:
+        print graph_stats
 
     x_labels = []
-    for x in range(5,65, 5):
-        x_labels.append(str(x))
+    for x_label in range(5, 65, 5):
+        x_labels.append(str(x_label))
 
-    if DEBUG: print x_labels
+    if DEBUG:
+        print x_labels
 
 
     # Create the graphs
-    if line_graph.twoline("pynet-rtr1-octets.svg", "pynet-rtr1 Fa4 Input/Output Bytes", graph_stats["in_octets"], 
-                "In Octets", graph_stats["out_octets"], "Out Octets", x_labels):
+    if line_graph.twoline("pynet-rtr1-octets.svg", "pynet-rtr1 Fa4 Input/Output Bytes",
+                          graph_stats["in_octets"], "In Octets", graph_stats["out_octets"],
+                          "Out Octets", x_labels):
         print "In/Out Octets graph created"
 
-    if line_graph.twoline("pynet-rtr1-pkts.svg", "pynet-rtr1 Fa4 Input/Output Unicast Packets", graph_stats["in_ucast_pkts"], 
-                "In Packets", graph_stats["out_ucast_pkts"], "Out Packets", x_labels):
+    if line_graph.twoline("pynet-rtr1-pkts.svg", "pynet-rtr1 Fa4 Input/Output Unicast Packets",
+                          graph_stats["in_ucast_pkts"], "In Packets", graph_stats["out_ucast_pkts"],
+                          "Out Packets", x_labels):
         print "In/Out Packets graph created"
 
     print
 
 
 if __name__ == '__main__':
-    main() 
+    main()
