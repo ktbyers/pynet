c9c2f619c169b603ef1e48b476a36ec4f6b74f7b
Kirk Byers <ktbyers@twb-tech.com>
2015-02-12 17:12:00 -0800

    pylint cleanup and restructuring

--- appliedpy_ecourse/class5/ex1_eapi_interfaces.py
+++ appliedpy_ecourse/class5/ex1_eapi_interfaces.py
@@ -1,37 +1,46 @@
 #!/usr/bin/env python
 '''
-Use Arista's eAPI to obtain 'show interfaces' from the switch.  Parse the 'show 
-interfaces' output to obtain the 'inOctets' and 'outOctets' fields for each of 
+Use Arista's eAPI to obtain 'show interfaces' from the switch.  Parse the 'show
+interfaces' output to obtain the 'inOctets' and 'outOctets' fields for each of
 the interfaces on the switch.  Accomplish this directly using jsonrpclib.
 '''
 
 import jsonrpclib
-from pprint import pprint
 
-arista_dict = dict(
-    ip = '10.10.10.10',
-    port = 8543,
-    username = 'eapi',
-    password = '*********',
-)
+def main():
+    '''
+    Use Arista's eAPI to obtain 'show interfaces' from the switch.
+    '''
 
-eapi_url = 'https://{username}:{password}@{ip}:{port}/command-api'.format(**arista_dict)
-eapi_conn = jsonrpclib.Server(eapi_url)
+    arista_dict = dict(
+        ip='50.242.94.227',
+        port=8243,
+        username='eapi',
+        password='99saturday'
+    )
 
-interfaces = eapi_conn.runCmds(1, ["show interfaces"])
+    eapi_url = 'https://{username}:{password}@{ip}:{port}/command-api'.format(**arista_dict)
+    eapi_conn = jsonrpclib.Server(eapi_url)
 
-# Strip off unneeded data structures
-interfaces = interfaces[0]['interfaces']
+    interfaces = eapi_conn.runCmds(1, ["show interfaces"])
 
-# inOctets/outOctets are fields inside 'interfaceCounters' dict
-data_stats = {}
-for interface,int_values in interfaces.items():
-    int_counters = int_values.get('interfaceCounters', {})
-    data_stats[interface] = (int_counters.get('inOctets'), int_counters.get('outOctets'))
+    # Strip off unneeded data structures
+    interfaces = interfaces[0]['interfaces']
 
-# Print output data
-print "\n{:20} {:<20} {:<20}".format("Interface:", "inOctets", "outOctets")
-for intf,octets in data_stats.items():
-    print "{:20} {:<20} {:<20}".format(intf, octets[0], octets[1])
+    # inOctets/outOctets are fields inside 'interfaceCounters' dict
+    data_stats = {}
+    for interface, int_values in interfaces.items():
+        int_counters = int_values.get('interfaceCounters', {})
+        data_stats[interface] = (int_counters.get('inOctets'), int_counters.get('outOctets'))
 
-print
+    # Print output data
+    print "\n{:20} {:<20} {:<20}".format("Interface:", "inOctets", "outOctets")
+    for intf, octets in data_stats.items():
+        print "{:20} {:<20} {:<20}".format(intf, octets[0], octets[1])
+
+    print
+
+
+if __name__ == '__main__':
+
+    main()
