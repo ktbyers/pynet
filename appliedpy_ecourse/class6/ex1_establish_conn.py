7b7423261a4df8f7ad62efe06c6c31e8d43c962b
Kirk Byers <ktbyers@twb-tech.com>
2015-02-19 10:45:31 -0800

    Minor pylint cleanup

--- appliedpy_ecourse/class6/ex1_establish_conn.py
+++ appliedpy_ecourse/class6/ex1_establish_conn.py
@@ -1,5 +1,5 @@
 '''
-Using the onepk_helper library establish a onePK connection to one of the two 
+Using the onepk_helper library establish a onePK connection to one of the two
 lab routers.  From this lab router obtain the product_id and SerialNo:
 
 >>> rtr_obj.net_element.properties.product_id
@@ -15,26 +15,26 @@ from onepk_helper import NetworkDevice
 def main():
 
     pynet_rtr1 = dict(
-        ip = '10.10.10.10',
-        username = 'pyclass',
-        password = '********',
-        pin_file = 'pynet-rtr1-pin.txt',
-        port = 15002
+        ip='10.10.10.10',
+        username='user1',
+        password='**********',
+        pin_file='pynet-rtr1-pin.txt',
+        port=15002
     )
 
     pynet_rtr2 = dict(
-        ip = '10.10.10.10',
-        username = 'pyclass',
-        password = '********',
-        pin_file = 'pynet-rtr2-pin.txt',
-        port = 8002
+        ip='10.10.10.10',
+        username='user1',
+        password='**********',
+        pin_file='pynet-rtr2-pin.txt',
+        port=8002
     )
 
     for a_rtr in (pynet_rtr1, pynet_rtr2):
 
         rtr_obj = NetworkDevice(**a_rtr)
         rtr_obj.establish_session()
-        
+
         print "\nConnection established to: {}".format(rtr_obj.net_element.properties.sys_name)
         print rtr_obj.net_element.properties.product_id
         print rtr_obj.net_element.properties.SerialNo
@@ -45,6 +45,4 @@ def main():
 
 
 if __name__ == "__main__":
-
     main()
-
