d20f2c3793136ab1e63e077eafecc51d87ec0bc5
Kirk Byers <ktbyers@twb-tech.com>
2015-08-26 17:04:03 -0700

    Class4, exercise1

---
+++ pyth_ans_ecourse/class4/ex1_paramiko.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+'''
+Use Paramiko to retrieve the entire 'show version' output.
+'''
+
+import paramiko
+import time
+from getpass import getpass
+
+MAX_BUFFER = 65535
+
+def clear_buffer(remote_conn):
+    '''
+    Clear any data in the receive buffer
+    '''
+    if remote_conn.recv_ready():
+        return remote_conn.recv(MAX_BUFFER)
+
+def disable_paging(remote_conn, cmd='terminal length 0'):
+    '''
+    Disable output paging (i.e. --More--)
+    '''
+    cmd = cmd.strip()
+    remote_conn.send(cmd + '\n')
+    time.sleep(1)
+    clear_buffer(remote_conn)
+
+def send_command(remote_conn, cmd='', delay=1):
+    '''
+    Send command down the channel. Retrieve and return the output.
+    '''
+    if cmd != '':
+        cmd = cmd.strip()
+    remote_conn.send(cmd + '\n')
+    time.sleep(delay)
+
+    if remote_conn.recv_ready():
+        return remote_conn.recv(MAX_BUFFER)
+    else:
+        return ''
+
+def main():
+    '''
+    Use Paramiko to retrieve the entire 'show version' output.
+    '''
+    ip_addr = raw_input("Enter IP address: ")
+    username = 'pyclass'
+    password = getpass()
+    port = 8022
+
+    remote_conn_pre = paramiko.SSHClient()
+    remote_conn_pre.load_system_host_keys()
+
+    remote_conn_pre.connect(ip_addr, port=port, username=username, password=password,
+                            look_for_keys=False, allow_agent=False)
+    remote_conn = remote_conn_pre.invoke_shell()
+
+    time.sleep(1)
+    clear_buffer(remote_conn)
+    disable_paging(remote_conn)
+
+    output = send_command(remote_conn, cmd='show version')
+    print output
+
+if __name__ == "__main__":
+    main()
