af95431f6c46dbb3eb10a1d2bcc94ea85a576304
Kirk Byers <ktbyers@twb-tech.com>
2015-09-17 13:41:33 -0700

    Class7, ex2

--- pyth_ans_ecourse/class7/eapi_vlan.py
+++ pyth_ans_ecourse/class7/eapi_vlan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-Using Arista's eapilib, create a script that allows you to add a VLAN (both the
+Using Arista's pyeapi, create a script that allows you to add a VLAN (both the
 VLAN ID and the VLAN name).  Your script should first check that the VLAN ID is
 available and only add the VLAN if it doesn't already exist.  Use VLAN IDs
 between 100 and 999.  You should be able to call the script from the command
@@ -14,16 +14,16 @@ If you call the script with the --remove option, the VLAN will be removed.
 
 Once again only remove the VLAN if it exists on the switch.  You will probably
 want to use Python's argparse to accomplish the argument processing.
-
-Note, in the lab environment, if you want to directly execute your script, you
-will need to use '#!/usr/bin/env python' at the top of the script (instead of
-'!#/usr/bin/python').
 '''
 
-import eapilib
-import jsonrpclib
+import pyeapi
 import argparse
 
+def pyeapi_result(output):
+    '''
+    Return the 'result' value from the pyeapi output
+    '''
+    return output[0]['result']
 
 def check_vlan_exists(eapi_conn, vlan_id):
     '''
@@ -31,23 +31,16 @@ def check_vlan_exists(eapi_conn, vlan_id):
 
     Return either vlan_name or False
     '''
-
     vlan_id = str(vlan_id)
-    command_str = 'show vlan id {}'.format(vlan_id)
-    commands = [command_str]
-
+    cmd = 'show vlan id {}'.format(vlan_id)
     try:
-        response = eapi_conn.run_commands(commands)
-        check_vlan = response[0]['vlans']
-        if check_vlan.get(vlan_id) is not None:
-            vlan_name = check_vlan[vlan_id]['name']
-            return vlan_name
-
-    except jsonrpclib.jsonrpc.ProtocolError:
+        response = eapi_conn.enable(cmd)
+        check_vlan = pyeapi_result(response)['vlans']
+        return check_vlan[vlan_id]['name']
+    except (pyeapi.eapilib.CommandError, KeyError):
         pass
 
     return False
-
 
 def configure_vlan(eapi_conn, vlan_id, vlan_name=None):
     '''
@@ -57,29 +50,18 @@ def configure_vlan(eapi_conn, vlan_id, vlan_name=None):
 
     Note, if the vlan already exists, then this will just set the vlan_name
     '''
-
     command_str1 = 'vlan {}'.format(vlan_id)
-    commands = [command_str1]
+    cmd = [command_str1]
     if vlan_name is not None:
         command_str2 = 'name {}'.format(vlan_name)
-        commands.append(command_str2)
-
-    return eapi_conn.config(commands)
-
+        cmd.append(command_str2)
+    return eapi_conn.config(cmd)
 
 def main():
     '''
     Add/remove vlans from Arista switch in an idempotent manner
     '''
-
-    arista_dict = dict(
-        hostname='10.10.10.10',
-        port=8243,
-        username='username',
-        password='********'
-    )
-
-    eapi_conn = eapilib.create_connection(**arista_dict)
+    eapi_conn = pyeapi.connect_to("pynet-sw3")
 
     # Argument parsing
     parser = argparse.ArgumentParser(
@@ -111,22 +93,17 @@ def main():
             eapi_conn.config([command_str])
         else:
             print "VLAN does not exist, no action required"
-
     else:
-
         if check_vlan:
             if vlan_name is not None and check_vlan != vlan_name:
                 print "VLAN already exists, setting VLAN name"
                 configure_vlan(eapi_conn, vlan_id, vlan_name)
             else:
                 print "VLAN already exists, no action required"
-
         else:
             print "Adding VLAN including vlan_name (if present)"
             configure_vlan(eapi_conn, vlan_id, vlan_name)
 
-
 if __name__ == "__main__":
-
     main()
 
