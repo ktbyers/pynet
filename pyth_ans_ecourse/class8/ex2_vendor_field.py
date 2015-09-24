2c95a33e6c5487d141b0c18ead61000a16c4a90b
Kirk Byers <ktbyers@twb-tech.com>
2015-09-24 16:15:08 -0700

    Class8 exercises

---
+++ pyth_ans_ecourse/class8/ex2_vendor_field.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+'''
+Set the vendor field for each NetworkDevice to the appropriate vendor. Save
+this field to the database.
+'''
+
+from net_system.models import NetworkDevice
+import django
+
+def main():
+    '''
+    Set the vendor field for each NetworkDevice to the appropriate vendor. Save
+    this field to the database.
+    '''
+    django.setup()
+    devices = NetworkDevice.objects.all()
+    for a_device in devices:
+        if 'cisco' in a_device.device_type:
+            a_device.vendor = 'Cisco'
+        elif 'juniper' in a_device.device_type:
+            a_device.vendor = 'Juniper'
+        elif 'arista' in a_device.device_type:
+            a_device.vendor = 'Arista'
+        a_device.save()
+
+    for a_device in devices:
+        print a_device, a_device.vendor
+
+if __name__ == "__main__":
+    main()
