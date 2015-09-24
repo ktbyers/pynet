c862b963210d8f33fbffa509d452cf5a3c9d653e
Kirk Byers <ktbyers@twb-tech.com>
2015-09-24 16:00:13 -0700

    Class8

---
+++ pyth_ans_ecourse/class8/ex5_db_show_version.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+'''
+Use Netmiko to connect to each of the devices in the database. Execute
+'show version' on each device. Record the amount of time required to do this.
+'''
+
+from netmiko import ConnectHandler
+from datetime import datetime
+
+from net_system.models import NetworkDevice
+import django
+
+def show_version(a_device):
+    '''
+    Execute show version command using Netmiko
+    '''
+    creds = a_device.credentials
+    remote_conn = ConnectHandler(device_type=a_device.device_type,
+                                 ip=a_device.ip_address,
+                                 username=creds.username,
+                                 password=creds.password,
+                                 port=a_device.port, secret='')
+    print
+    print '#' * 80
+    print remote_conn.send_command("show version")
+    print '#' * 80
+    print
+
+def main():
+    '''
+    Use Netmiko to connect to each of the devices in the database. Execute
+    'show version' on each device.
+    '''
+    django.setup()
+
+    start_time = datetime.now()
+    devices = NetworkDevice.objects.all()
+    for a_device in devices:
+        show_version(a_device)
+
+    elapsed_time = datetime.now() - start_time
+    print "Elapsed time: {}".format(elapsed_time)
+
+if __name__ == "__main__":
+    main()
---
+++ pyth_ans_ecourse/class8/ex6_threads_show_ver.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+'''
+Use threads and Netmiko to connect to each of the devices in the database. Execute
+'show version' on each device. Record the amount of time required to do this.
+'''
+
+from netmiko import ConnectHandler
+from datetime import datetime
+
+from net_system.models import NetworkDevice
+import django
+
+import threading
+
+
+def show_version(a_device):
+    '''
+    Execute show version command using Netmiko
+    '''
+    creds = a_device.credentials
+    remote_conn = ConnectHandler(device_type=a_device.device_type,
+                                 ip=a_device.ip_address,
+                                 username=creds.username,
+                                 password=creds.password,
+                                 port=a_device.port, secret='')
+    print
+    print '#' * 80
+    print remote_conn.send_command("show version")
+    print '#' * 80
+    print
+
+def main():
+    '''
+    Use threads and Netmiko to connect to each of the devices in the database. Execute
+    'show version' on each device. Record the amount of time required to do this.
+    '''
+    django.setup()
+    start_time = datetime.now()
+    devices = NetworkDevice.objects.all()
+
+    for a_device in devices:
+        my_thread = threading.Thread(target=show_version, args=(a_device,))
+        my_thread.start()
+
+    main_thread = threading.currentThread()
+    for some_thread in threading.enumerate():
+        if some_thread != main_thread:
+            print some_thread
+            some_thread.join()
+
+    print "\nElapsed time: " + str(datetime.now() - start_time)
+
+if __name__ == "__main__":
+    main()
