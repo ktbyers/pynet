f63d13c1dc9328ccebf1a31552a6d3432b57e52c
Kirk Byers <ktbyers@twb-tech.com>
2015-10-01 17:32:07 -0700

    Class9, exercise3

---
+++ pyth_ans_ecourse/class9/exercise3/mytest/world.py
@@ -0,0 +1,9 @@
+'''
+Python class on writing reusable code
+'''
+def func1():
+    '''Simple test function'''
+    print "Hello world"
+
+if __name__ == "__main__":
+    print "Main program - world"
