79fa98efd6e1eeb16aea598dec62b469ae8d4170
Kirk Byers <ktbyers@twb-tech.com>
2015-11-05 17:10:12 -0800

    Fix credentials issues

--- django/load_devices.py
+++ django/load_devices.py
@@ -19,18 +19,6 @@ def main():
     arista_group = InventoryGroup.objects.get_or_create(group_name='arista')
     print arista_group
 
-    # Create four switch objects
-    for switch_name, ssh_port, ip_addr in my_switches:
-        switch_obj = NetworkSwitch.objects.get_or_create(
-            device_name=switch_name,
-            device_type='arista_eos',
-            ip_address=ip_addr,
-            management_ip=management_ip,
-            port=ssh_port,
-            group_name=arista_group[0]
-        )
-        print switch_obj
-
     # Create credential object
     arista_creds = Credentials.objects.get_or_create(
         username='admin1',
@@ -39,12 +27,18 @@ def main():
     )
     print arista_creds
 
-    # Bind switches to credentials
-    net_devices = NetworkSwitch.objects.all()
-    print net_devices
-    for my_switch in net_devices:
-        my_switch.credentials = arista_creds[0]
-        my_switch.save()
+    # Create four switch objects
+    for switch_name, ssh_port, ip_addr in my_switches:
+        switch_obj = NetworkSwitch.objects.get_or_create(
+            device_name=switch_name,
+            device_type='arista_eos',
+            ip_address=ip_addr,
+            management_ip=management_ip,
+            port=ssh_port,
+            group_name=arista_group[0],
+            credentials = arista_creds[0],
+        )
+        print switch_obj
 
 if __name__ == "__main__":
     main()
