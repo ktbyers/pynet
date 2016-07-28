f7b269f564352c269cbe893eeb7bf4d3d09164fb
Kirk Byers <ktbyers@twb-tech.com>
2016-07-27 20:17:25 -0700

    update SNMP script

--- pyth_ans_ecourse/class2/ex4_snmp.py
+++ pyth_ans_ecourse/class2/ex4_snmp.py
@@ -3,9 +3,8 @@
 Create a script that connects to both routers (pynet-rtr1 and pynet-rtr2) and
 prints out both the MIB2 sysName and sysDescr.
 '''
-
-import snmp_helper
 import getpass
+import snmp_helper
 
 SYS_DESCR = '1.3.6.1.2.1.1.1.0'
 SYS_NAME = '1.3.6.1.2.1.1.5.0'
@@ -15,15 +14,14 @@ def main():
     Create a script that connects to both routers (pynet-rtr1 and pynet-rtr2) and
     prints out both the MIB2 sysName and sysDescr.
     '''
-    ip_addr = raw_input("IP address: ")
-    ip_addr = ip_addr.strip()
+    ip_addr1 = raw_input("pynet-rtr1 IP address: ")
+    ip_addr2 = raw_input("pynet-rtr2 IP address: ")
     community_string = getpass.getpass(prompt="Community string: ")
 
-    pynet_rtr1 = (ip_addr, community_string, 7961)
-    pynet_rtr2 = (ip_addr, community_string, 8061)
+    pynet_rtr1 = (ip_addr1, community_string, 161)
+    pynet_rtr2 = (ip_addr2, community_string, 161)
 
     for a_device in (pynet_rtr1, pynet_rtr2):
-
         print "\n*********************"
         for the_oid in (SYS_NAME, SYS_DESCR):
             snmp_data = snmp_helper.snmp_get_oid(a_device, oid=the_oid)
@@ -31,7 +29,6 @@ def main():
 
             print output
         print "*********************"
-
     print
 
 if __name__ == "__main__":
