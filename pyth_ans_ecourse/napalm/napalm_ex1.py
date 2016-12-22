ef159e6d1ffc8a65a1819bed51e94f704fb5b08a
Kirk Byers <ktbyers@twb-tech.com>
2016-12-22 01:59:25 -0800

    ex1

--- pyth_ans_ecourse/napalm/napalm_ex1.py
+++ pyth_ans_ecourse/napalm/napalm_ex1.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-from pprint import pprint as pp
-
+"""
+Connect to set of network devices using NAPALM (different platforms); print
+out the facts.
+"""
 from napalm_base import get_network_driver
 from my_devices import device_list
 
 def main():
+    """
+    Connect to set of network devices using NAPALM (different platforms); print
+    out the facts.
+    """
     for a_device in device_list:
         device_type = a_device.pop('device_type')
         driver = get_network_driver(device_type)
