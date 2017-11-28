8c4f217cd5cec7dbfe3d4fd3140588d4bbb9e702
Kirk Byers <ktbyers@twb-tech.com>
2017-11-28 13:28:18 -0800

    Week1 exercises

--- learning_python/lesson1/exercise1.py
+++ learning_python/lesson1/exercise1.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-"""
-Create three variables: ip_addr1, ip_addr2, ip_addr3 representing three IP addresses.
+"""Create a Python script that has three variables: ip_addr1, ip_addr2, ip_addr3 (representing
+three corresponding IP addresses). Print these three variables to standard output using a single
+print statement.
 
-print these three variables to standard output using a single print statement.
+Make your print statement compatible with both Python2 and Python3.
 
-Make your print statement compatible with both Python2 and Python2 (i.e. you should be able
-to run this same program using either PY2 or PY3).
-
-If you are using Linux or MacOS make your program executable by adding a shebang line and
-by changing the files permissions (i.e. chmod 755 <file_name.py>)
+If you are using either Linux or MacOS make your program executable by adding a shebang line and
+by changing the files permissions (i.e. chmod 755 exercise1.py).
 """
 from __future__ import print_function
 
--- learning_python/lesson1/exercise2.py
+++ learning_python/lesson1/exercise2.py
@@ -1,24 +1,20 @@
-"""
-Prompt a user to enter in an IP address from standard input
-
-Read the IP address in and break the IP address up into its octets
-
-Print out the octet in decimal
+#!/usr/bin/env python
+"""Prompt a user to enter in an IP address from standard input. Read the IP address in and break it
+up into its octets. Print out the octets in decimal, binary, and hex.
 
 Your program output should look like the following:
 
-$ python exercise2.py 
+$ python exercise2.py
 Please enter an IP address: 80.98.100.240
 
-    Octet1         Octet2         Octet3         Octet4     
---------------------------------------------------------------------------------
-      80             98             100            240      
-   0b1010000      0b1100010      0b1100100     0b11110000   
-     0x50           0x62           0x64           0xf0      
---------------------------------------------------------------------------------
+    Octet1         Octet2         Octet3         Octet4
+------------------------------------------------------------
+      80             98             100            240
+   0b1010000      0b1100010      0b1100100     0b11110000
+     0x50           0x62           0x64           0xf0
+------------------------------------------------------------
 
-4-columns 15 wide
-header line
+Four columns, fifteen characters wide, a header column, data centered in the column.
 
 """
 from __future__ import print_function
@@ -33,9 +29,11 @@ except NameError:
 octets = ip_addr.split(".")
 print()
 print("{:^15}{:^15}{:^15}{:^15}".format("Octet1", "Octet2", "Octet3", "Octet4"))
-print("-" * 80)
+print("-" * 60)
 print("{:^15}{:^15}{:^15}{:^15}".format(*octets))
-print("{:^15}{:^15}{:^15}{:^15}".format(bin(int(octets[0])), bin(int(octets[1])), bin(int(octets[2])), bin(int(octets[3]))))
-print("{:^15}{:^15}{:^15}{:^15}".format(hex(int(octets[0])), hex(int(octets[1])), hex(int(octets[2])), hex(int(octets[3]))))
-print("-" * 80)
+print("{:^15}{:^15}{:^15}{:^15}".format(bin(int(octets[0])), bin(int(octets[1])),
+                                        bin(int(octets[2])), bin(int(octets[3]))))
+print("{:^15}{:^15}{:^15}{:^15}".format(hex(int(octets[0])), hex(int(octets[1])),
+                                        hex(int(octets[2])), hex(int(octets[3]))))
+print("-" * 60)
 print()
