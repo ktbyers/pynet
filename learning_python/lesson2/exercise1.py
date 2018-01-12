b06f364be7d4644058a5f138fec1f131721d7518
Kirk Byers <ktbyers@twb-tech.com>
2018-01-12 14:13:25 -0800

    Finalizing exercises for week2

--- learning_python/lesson2/exercise1.py
+++ learning_python/lesson2/exercise1.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
-"""
-Open the "show_version.txt" file for reading. Use the .read() method to read in the entire
-file contents to a variable. Print out the file contents to the screen. Also print out the
-type of the variable (you should have a string at this point).
+"""Open the "show_version.txt" file for reading. Use the .read() method to read in the entire file
+contents to a variable. Print out the file contents to the screen. Also print out the type of the
+variable (you should have a string at this point).
 
 Close the file.
 
 Open the file a second time using a Python context manager (with statement). Read in the file
-contents using the readlines() method. Print out the file contents to the screen. Also print out
+contents using the .readlines() method. Print out the file contents to the screen. Also print out
 the type of the variable (you should have a list at this point).
 """
 from __future__ import print_function, unicode_literals
 
+banner = '-' * 80
+
 f = open("show_version.txt")
 show_ver = f.read()
+
+print(banner)
 print(show_ver)
+print(banner)
 print(type(show_ver))
+print(banner)
 f.close()
 
 with open("show_version.txt") as f:
     show_ver = f.readlines()
 
+print("\n" + banner)
 print(show_ver)
+print(banner)
 print(type(show_ver))
+print(banner + "\n")
--- learning_python/lesson2/exercise2.py
+++ learning_python/lesson2/exercise2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-"""
-Create a list of five IP addresses.
+"""Create a list of five IP addresses.
 
 Use the .append() method to add an IP address onto the end of the list. Use the .extend()
 method to add two more IP addresses to the end of the list.
@@ -10,7 +9,7 @@ Use list concatenation to add two more IP addresses to the end of the list.
 Print out the entire list of ip addresses. Print out the first IP address in the list. Print out
 the last IP address in the list.
 
-Using the pop() method remove the first IP address in the list;; remove the last IP address in
+Using the .pop() method to remove the first IP address in the list and the last IP address in
 the list.
 
 Update the new first IP address in the list to be '2.2.2.2'. Print out the new first IP address
