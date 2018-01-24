0947f2f3d088b161e41483cff2a1f5d1e2ba4d34
Kirk Byers <ktbyers@twb-tech.com>
2018-01-24 10:50:49 -0800

    Updating class3 exercises

--- learning_python/lesson3/exercise3.py
+++ learning_python/lesson3/exercise3.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 '''
-Read in the 'show_lldp_neighbors_detail.txt' file. Use a while loop to keep reading the file
-contents until you have encountered the remote "system Name" and remote "Port id". After you
-have both of those break out of the while loop and save the two items into the system_name and
-port_id variables.
+Read the 'show_lldp_neighbors_detail.txt' file. Loop over the lines of this file. Keep reading the
+lines until you have encountered the remote "System Name" and remote "Port id". Save these two items
+into variables and print them to the screen. You should extract only the system name and port id
+from the lines (i.e. your variables should only have 'twb-sf-hpsw1' and '15'). Break out of your
+loop once you have retrieved these two items.
 '''
+from __future__ import unicode_literals, print_function
 
 
 with open("show_lldp_neighbors_detail.txt") as f:
-    show_lldp = f.readlines()
+    show_lldp = f.read()
 
 system_name, port_id = (None, None)
-while True:
-    line = show_lldp.pop(0)
-    line = line.strip()
+for line in show_lldp.splitlines():
     if 'System Name: ' in line:
         _, system_name = line.split('System Name: ')
     elif 'Port id: ' in line:
--- learning_python/lesson3/exercise4.py
+++ learning_python/lesson3/exercise4.py
@@ -1,34 +1,63 @@
-from pprint import pprint
-import os
+#!/usr/bin/env python
+"""
+You have the following data structure:
+arp_table = [('10.220.88.1', '0062.ec29.70fe'),
+ ('10.220.88.20', 'c89c.1dea.0eb6'),
+ ('10.220.88.21', '1c6a.7aaf.576c'),
+ ('10.220.88.28', '5254.aba8.9aea'),
+ ('10.220.88.29', '5254.abbe.5b7b'),
+ ('10.220.88.30', '5254.ab71.e119'),
+ ('10.220.88.32', '5254.abc7.26aa'),
+ ('10.220.88.33', '5254.ab3a.8d26'),
+ ('10.220.88.35', '5254.abfb.af12'),
+ ('10.220.88.37', '0001.00ff.0001'),
+ ('10.220.88.38', '0002.00ff.0001'),
+ ('10.220.88.39', '6464.9be8.08c8'),
+ ('10.220.88.40', '001c.c4bf.826a'),
+ ('10.220.88.41', '001b.7873.5634')]
 
-# Toggle this to use Windows
-WINDOWS = False
+Loop over this data structure and extract all of the MAC addresses. Process all of the MAC
+addresses to get them into a standard format. Print all of the new standardized MAC address to the
+screen. The standardized format should be as follows:
 
-base_ip = '8.8.4.'
-base_cmd_linux = 'ping -c 2'
-base_cmd_windows = 'ping -n 2'
-# Ternary operator
-base_cmd = base_cmd_windows if WINDOWS else base_cmd_linux
+00:62:EC:29:70:FE
 
-ip_list = []
-for last_octet in range(1, 255):
-    new_ip = base_ip + str(last_octet)
-    ip_list.append(new_ip)
+The hex digits should be capitalized. Additionally, there should be a colon between each octet in
+the MAC address.
+"""
+from __future__ import unicode_literals, print_function
 
-for i, ip_addr in enumerate(ip_list):
-    print("{} ---> {}".format(i, ip_addr))
+arp_table = [
+ ('10.220.88.1', '0062.ec29.70fe'),
+ ('10.220.88.20', 'c89c.1dea.0eb6'),
+ ('10.220.88.21', '1c6a.7aaf.576c'),
+ ('10.220.88.28', '5254.aba8.9aea'),
+ ('10.220.88.29', '5254.abbe.5b7b'),
+ ('10.220.88.30', '5254.ab71.e119'),
+ ('10.220.88.32', '5254.abc7.26aa'),
+ ('10.220.88.33', '5254.ab3a.8d26'),
+ ('10.220.88.35', '5254.abfb.af12'),
+ ('10.220.88.37', '0001.00ff.0001'),
+ ('10.220.88.38', '0002.00ff.0001'),
+ ('10.220.88.39', '6464.9be8.08c8'),
+ ('10.220.88.40', '001c.c4bf.826a'),
+ ('10.220.88.41', '001b.7873.5634')]
 
-ip_list = ip_list[2:6]
+print()
+for ip_addr, mac_addr in arp_table:
+    # Eliminate the period and convert to upper case
+    mac_addr = mac_addr.split(".")
+    mac_addr = "".join(mac_addr)
+    mac_addr = mac_addr.upper()
 
-if '8.8.4.4' in ip_list:
-    print("FOUND 8.8.4.4")
-else:
-    print("FAILED")
+    # Process two hex digits at a time
+    new_mac = []
+    while len(mac_addr) > 0:
+        entry = mac_addr[:2]
+        mac_addr = mac_addr[2:]
+        new_mac.append(entry)
 
-print('-' * 80)
-for ip_addr in ip_list:
-    if '8.8.4.0' == ip_addr:
-        continue
-    print("IP Addr: ", ip_addr)
-    return_code = os.system("ping -c 3 {}".format(ip_addr))
-    print('-' * 80)
+    # Reunite MAC address using a colon
+    new_mac = ":".join(new_mac)
+    print(new_mac)
+print()
