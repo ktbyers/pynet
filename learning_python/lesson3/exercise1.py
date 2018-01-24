cb5da759ddb0ce42f1fe9f2f53d11ade025d988e
Kirk Byers <ktbyers@twb-tech.com>
2018-01-24 10:30:30 -0800

    Minor cleanup of class3 exercises

--- learning_python/lesson3/exercise1.py
+++ learning_python/lesson3/exercise1.py
@@ -2,7 +2,7 @@
 """
 Read the "show_vlan.txt" file into your program. Loop through the lines in this file and extract
 all of the VLAN_ID, VLAN_NAME combinations. From these VLAN_ID and VLAN_NAME construct a new list
-where each element in the list is a tuple consisting of (VLAN_ID, VLAN_NAME). Print this data 
+where each element in the list is a tuple consisting of (VLAN_ID, VLAN_NAME). Print this data
 structure to the screen. Your output should look as follows:
 
 [('1', 'default'),
--- learning_python/lesson3/exercise2.py
+++ learning_python/lesson3/exercise2.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+"""
+Read the contents of the "show_arp.txt" file. Using a for loop, iterate over the lines of this
+file. Process the lines of the file and separate out the ip_addr and mac_addr for each entry into a
+separate variable.
+
+Add a conditional statement that searches for '10.220.88.1'. If 10.220.88.1 is found, print out the
+string "Default gateway IP/Mac" and the corresponding IP address and MAC Address.
+
+Using a conditional statement, also search for '10.220.88.30'. If this IP address is found, then
+print out "Arista3 IP/Mac is" and the corresponding ip_addr and mac_addr.
+
+Keep track of whether you have found both the Default Gateway and the Arista3 switch. Once you have
+found both of these devices, 'break' out of the for loop.
+"""
+from __future__ import unicode_literals, print_function
 
 with open("show_arp.txt") as f:
     show_arp = f.read()
@@ -15,11 +30,11 @@ for line in show_arp.splitlines():
         print("Default gateway IP/Mac is: {}/{}".format(ip_addr, mac_addr))
         found1 = True
     elif ip_addr == '10.220.88.30':
-        print("arista3 IP/Mac is: {}/{}".format(ip_addr, mac_addr))
+        print("Arista3 IP/Mac is: {}/{}".format(ip_addr, mac_addr))
         found2 = True
 
     if found1 and found2:
-        print("Exitting...")
+        print("Exiting...")
         break
 
 print()
