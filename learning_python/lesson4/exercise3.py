1c3e0ba99179314a0be0ff4483a50bc469652d8f
Kirk Byers <ktbyers@twb-tech.com>
2018-01-31 16:18:39 -0800

    Add exercise3

---
+++ learning_python/lesson4/exercise3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Read in the "show_version.txt" file. From this file use regular expressions to extract the
+os_version, serial_number, and configuration register value.
+
+Your output should look as follows:
+
+          OS Version: 15.4(2)T1      
+       Serial Number: FTX0000038X    
+     Config Register: 0x2102 
+
+"""
+from __future__ import unicode_literals, print_function
+import re
+
+with open("show_version.txt") as f:
+    show_ver = f.read()
+
+match = re.search(r"^Cisco IOS Software,.* Version (.*),", show_ver, flags=re.M)
+if match:
+    os_version = match.group(1)
+
+match = re.search(r"^Processor board ID (.*)\s*$", show_ver, flags=re.M)
+if match:
+    serial_number = match.group(1)
+
+match = re.search(r"^Configuration register is (.*)\s*$", show_ver, flags=re.M)
+if match:
+    config_register = match.group(1)
+
+print()
+print("{:>20}: {:15}".format("OS Version", os_version))
+print("{:>20}: {:15}".format("Serial Number", serial_number))
+print("{:>20}: {:15}".format("Config Register", config_register))
+print()
