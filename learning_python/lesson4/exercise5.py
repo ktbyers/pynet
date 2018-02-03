e9e56f1ff6c99e49fc0b8e8785444e25795d085a
Kirk Byers <ktbyers@twb-tech.com>
2018-02-02 17:12:07 -0800

    Minor docstring change

--- learning_python/lesson4/exercise5.py
+++ learning_python/lesson4/exercise5.py
@@ -9,10 +9,10 @@ The two relevant IPv6 addresses you need to extract are:
     2001:11:2233::a1/24
     2001:cc11:22bb:0:2ec2:60ff:fe4f:feb2/64
 
-Try to use re.DOTALL in your pattern search. Your search pattern should not include
-any of the literal characters in the IPv6 address.
+Try to use re.DOTALL flag as part of your search. Your search pattern should not include any of
+the literal characters in the IPv6 address.
 
-Create a list of IPv6 addresses that includes only the above two addresses.
+From this, create a list of IPv6 addresses that includes only the above two addresses.
 
 Print this list to the screen.
 """
