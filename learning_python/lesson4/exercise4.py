8879c6f0f05948287d9d0ae1832235561c487813
Kirk Byers <ktbyers@twb-tech.com>
2018-02-02 17:08:41 -0800

    Updating exercise text description

--- learning_python/lesson4/exercise4.py
+++ learning_python/lesson4/exercise4.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Use a named regular expression (?P<name>) to extract the model from the below string.
+Using a named regular expression (?P<name>) extract the model from the below string.
 
 show_version = '''
 Cisco 881 (MPC8300) processor (revision 1.0) with 236544K/25600K bytes of memory.
@@ -12,13 +12,13 @@ Processor board ID FTX0000038X
 126000K bytes of ATA CompactFlash (Read/Write)
 '''
 
-Note, in this example '881' is the relevant module. Your regular expression should not,
-however, include '881' in its search pattern since this number changes across devices.
+Note, in this example '881' is the relevant model. Your regular expression should not, however,
+include '881' in its search pattern since this number changes across devices.
 
-Use a named regular expression to also extract the "236544K/25600K" memory string.
+Using a named regular expression, also extract the "236544K/25600K" memory string.
 
-Once again none of the actual digits of the memory on this device should be used in the
-regular expression search pattern.
+Once again none of the actual digits of the memory on this device should be used in the regular
+expression search pattern.
 
 Print both the model number and the memory string to the screen.
 """
