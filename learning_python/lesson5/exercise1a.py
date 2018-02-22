61ede01d7e1b72441b7603892dc21ec85f478c5f
Kirk Byers <ktbyers@twb-tech.com>
2018-02-22 10:58:07 -0800

    lesson5 exercises

---
+++ learning_python/lesson5/exercise1a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Create an ssh_conn function. This function should have three parameters: ip_addr, username, and
+password. The function should print out each of these three variables and clearly indicate which
+variable it is printing out.
+
+Call this ssh_conn function using entirely positional arguments.
+
+Call this ssh_conn function using entirely named arguments.
+
+Call this ssh_conn function using a mix of positional and named arguments.
+"""
+from __future__ import print_function, unicode_literals
+
+
+def ssh_conn(ip_addr, username, password):
+    print("-" * 80)
+    print("IP Addr: {}".format(ip_addr))
+    print("Username: {}".format(username))
+    print("Password: {}".format(password))
+    print("-" * 80)
+
+
+# Positional args
+ssh_conn('192.168.1.1', 'admin', 'cisco123')
+
+# Named args
+ssh_conn(ip_addr='192.168.1.1', username='admin', password='cisco123')
+
+# Mixing named and positional args
+ssh_conn('192.168.1.1', password='cisco123', username='admin1')
---
+++ learning_python/lesson5/exercise1b.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Expand on the ssh_conn function from exercise1 except add a fourth parameter 'device_type' with
+a default value of 'cisco_ios'. Print all four of the function variables out as part of the
+function's execution.
+
+Call the 'ssh_conn2' function both with and without specifying the device_type
+
+Create a dictionary that maps to the function's parameters. Call this ssh_conn2 function using
+the **kwargs technique.
+"""
+from __future__ import print_function, unicode_literals
+
+
+def ssh_conn2(ip_addr, username, password, device_type="cisco_ios"):
+    print("-" * 80)
+    print("IP Addr: {}".format(ip_addr))
+    print("Username: {}".format(username))
+    print("Password: {}".format(password))
+    print("Platform: {}".format(device_type))
+    print("-" * 80)
+
+
+# Adding a default value
+ssh_conn2('192.168.1.1', password='cisco123', username='admin1')
+ssh_conn2('192.168.1.1', password='cisco123', username='admin1', device_type="cisco_nxos")
+
+# Calling using a dictionary
+my_device = {
+    "ip_addr": "172.16.1.1",
+    "device_type": "cisco_xr",
+    "username": "admin",
+    "password": "cisco123"
+}
+ssh_conn2(**my_device)
