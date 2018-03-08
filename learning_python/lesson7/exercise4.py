886fbddd9d906bcdd9ea93f43ae488452c949403
Kirk Byers <ktbyers@twb-tech.com>
2018-03-08 13:19:58 -0800

    Minor cleanup ex4, class7

--- learning_python/lesson7/exercise4.py
+++ learning_python/lesson7/exercise4.py
@@ -12,8 +12,9 @@ Take the YAML file and corresponding data structure that you defined in exercise
 }
 
 From this YAML data input source, use Jinja templating to generate the following configuration
-output
+output:
 
+----
 interface Ethernet1
   switchport mode access
   switchport access vlan 10
@@ -24,16 +25,23 @@ interface Ethernet3
   switchport mode trunk
   switchport trunk native vlan 1
   switchport trunk allowed vlan all
+----
 
-The following should all be variables in your Jinja template:
+The following should all be variables in your Jinja template (the names may be different than
+below, but they should be variabilized and not be hard-coded in your template).
 
+----
 interface_name
 switchport_mode
 access_vlan
 native_vlan
-trunk_allowed_vlans
+trunk_vlans
+----
 
-All your Jinja2 variables should be retrieved from your YAML file
+All your Jinja2 variables should be retrieved from your YAML file.
+
+This exercise might be challenging.
+
 """
 from __future__ import print_function, unicode_literals
 import yaml
