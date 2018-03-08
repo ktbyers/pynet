55f7714dd03bdd73d7177a597fa45a7b6f784a92
Kirk Byers <ktbyers@twb-tech.com>
2018-03-08 12:51:31 -0800

    class7 exercises

---
+++ learning_python/lesson7/exercise1c.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+Using Jinja2 templating and a for-loop inside the template, generate the following configuration
+snippet:
+
+vlan 501
+   name blue501
+vlan 502
+   name blue502
+vlan 503
+   name blue503
+vlan 504
+   name blue504
+vlan 505
+   name blue505
+vlan 506
+   name blue506
+vlan 507
+   name blue507
+vlan 508
+   name blue508
+
+
+Your template should be inside your Python program for simplicity.
+
+It is fine for your VLAN IDs to be out of order in the generated configuration (for example, VLAN
+ID 508 can come before VLAN ID 504).
+
+"""
+from __future__ import print_function, unicode_literals
+import jinja2
+
+my_vlans = {
+    '501': 'blue501',
+    '502': 'blue502',
+    '503': 'blue503',
+    '504': 'blue504',
+    '505': 'blue505',
+    '506': 'blue506',
+    '507': 'blue507',
+    '508': 'blue508',
+}
+template_vars = {
+    'vlans': my_vlans
+}
+
+vlan_template = '''
+
+{%- for vlan_id, vlan_name in vlans.items() %}
+vlan {{ vlan_id }}
+   name {{ vlan_name }}
+{%- endfor %}
+
+'''
+
+template = jinja2.Template(vlan_template)
+print(template.render(template_vars))
--- learning_python/lesson7/exercise2.py
+++ learning_python/lesson7/exercise2.py
@@ -2,6 +2,7 @@
 """
 Using Python and Jinja2 templating generate the following OSPF configuration:
 
+----
 interface vlan 1
    ip ospf priority 100
 
@@ -13,22 +14,30 @@ router ospf 10
    network 10.10.20.0/24 area 0.0.0.0
    network 10.10.30.0/24 area 0.0.0.0
    max-lsa 12000
+----
 
 The following items should be variables in your Jinja2 template:
+
+----
 ospf_process_id
 ospf_priority
 ospf_active_interfaces (i.e. the non-passive interfaces)
 ospf_area0_networks (the three networks that are specified as belonging to area0)
+----
 
 Your template should be in an external file.
 
 Your template should also use a conditional to control whether this is output or not:
 
+----
 interface vlan 1
    ip ospf priority 100
 
-if the 'ospf_priority variable is defined', then include that. If it is not defined then
-only include the 'router ospf 10' section.
+----
+
+If the 'ospf_priority variable is defined', then include that section. If 'ospf_priority' is not
+defined then only include the 'router ospf 10' section.
+
 """
 from __future__ import print_function, unicode_literals
 import jinja2
