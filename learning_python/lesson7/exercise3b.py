ab6c6ada68b193cfa3771292dfe5c6cfca12a543
Kirk Byers <ktbyers@twb-tech.com>
2018-03-19 14:58:11 -0700

    Minor update to class7 exercise

--- learning_python/lesson7/exercise3b.py
+++ learning_python/lesson7/exercise3b.py
@@ -5,7 +5,9 @@ key that refers to a dictionary.
 
 Use Python to read in this YAML data structure and print this to the screen.
 
-The output of your Python script should look as follows:
+The output of your Python script should look as follows (in other words, your YAML data structure
+should yield the following when read by Python). You YAML data structure should be written in
+expanded YAML format.
 
 {'interfaces': {
     'Ethernet1': {'mode': 'access', 'vlan': 10},
@@ -13,7 +15,7 @@ The output of your Python script should look as follows:
     'Ethernet3': {'mode': 'trunk',
                   'native_vlan': 1,
                   'trunk_vlans': 'all'}
-    }
+    }
 }
 
 """
