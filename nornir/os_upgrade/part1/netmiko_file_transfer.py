0fd0d4ce26b17912ab5882d5cc0b0aa65855731f
Carl <carlniger@gmail.com>
2019-05-01 14:46:03 -0700

    updating for rev to 2.x blog post

--- nornir/os_upgrade/part1/netmiko_file_transfer.py
+++ nornir/os_upgrade/part1/netmiko_file_transfer.py
@@ -1,15 +1,15 @@
-from nornir.core import InitNornir
+from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_file_transfer
 
 from nornir_utilities import nornir_set_creds, std_print
 
 
-# Initialize Nornir object using hosts.yaml and groups.yaml
-norn = InitNornir(config_file="nornir.yml")
-nornir_set_creds(norn)
+# Initialize Nornir object using default "SimpleInventory" plugin 
+nr = InitNornir()
+nornir_set_creds(nr)
 test_file = 'test_file4.txt'
 
-result = norn.run(
+result = nr.run(
     task=netmiko_file_transfer,
     source_file=test_file,
     dest_file=test_file,
