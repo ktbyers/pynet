2c33053b03d19c0885f37645b526cf4c4dea285c
Kirk Byers <ktbyers@twb-tech.com>
2021-06-25 12:20:09 -0700

    Adding send_multiline/send_multiline_timing examples for netmiko4

---
+++ netmiko4/send_multiline/old_solution_timing/delete_file.py
@@ -0,0 +1,25 @@
+from getpass import getpass
+from netmiko import ConnectHandler
+
+device = {
+    "host": "cisco3.lasthop.io",
+    "username": "pyclass",
+    "password": getpass(),
+    "device_type": "cisco_xe",
+}
+
+with ConnectHandler(**device) as net_connect:
+
+    filename = "test-reuben.txt"
+    cmd = f"del flash:/{filename}"
+
+    output = net_connect.send_command_timing(
+        cmd, strip_prompt=False, strip_command=False
+    )
+    output += net_connect.send_command_timing(
+        "\n", strip_prompt=False, strip_command=False
+    )
+    output += net_connect.send_command_timing(
+        "y", strip_prompt=False, strip_command=False
+    )
+    print(output)
